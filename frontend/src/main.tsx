2ae2201264d3ec48018a09ee3fa5eca0ba3c1cbe
Zedmat <104870914+harshkasat@users.noreply.github.com>
2025-03-17 00:04:59 +0530

    update : remove the dev mode from clerk login

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,10 +5,12 @@ import App from './App.tsx';
 import './index.css';
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+console.log(PUBLISHABLE_KEY)
 
 if (!PUBLISHABLE_KEY) {
   throw new Error("Missing Publishable Key")
 }
+console.log(PUBLISHABLE_KEY)
 
 createRoot(document.getElementById('root')!).render(
   <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/" 
